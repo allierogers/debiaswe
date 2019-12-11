ba6adc31eae6fc4f847845d4df8b7e5de19c3bb2
Allie Rogers <arogers@MV1-ML-2573.local>
2019-12-10 21:11:56 -0800

    start flask app

--- bias_explorer.py
+++ bias_explorer.py
@@ -1,33 +1,63 @@
+import numpy as np
+import os
+
 from argparse import ArgumentParser
+from flask import Flask, flash, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
 
 from debiaswe.we import WordEmbedding
+import model
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt'}
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            if not os.path.exists(app.config['UPLOAD_FOLDER']):
+                os.mkdir(app.config['UPLOAD_FOLDER'])
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+        
+#         E = WordEmbedding('embeddings/w2v_gnews_small.txt')
+        
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 
-def compute_bias_direction(rep_words):
-    """ Get the subspace that we will use to represent the bias. 
-    """
-    words_group1 = [words[2 * i] for i in range(len(args.rep_words) // 2)]
-    words_group2 = [words[2 * i + 1] for i in range(len(args.rep_words) // 2)]
-    
-    vs = [sum(E.v(w) for w in words) for words in (words_group2, words_group1)]
-    vs = [v / np.linalg.norm(v) for v in vs]
-
-    v_protected = vs[1] - vs[0]
-    v_protected = v_protected / np.linalg.norm(v_protected)
-    
-    return v_protected
 
 
-def main():
-    parser = ArgumentParser()
-    parser.add_argument('--we_path')
-    parser.add_argument('--rep_words', default=None)
-    args = parser.parse_args()
-    
-    # Load word embedding from provided filepath
-    E = WordEmbedding(args.we_path)
-    
-    v_protected = compute_bias_direction(args.rep_words)
-
-    # Analogies based on the protected direction
-    a_protected = E.best_analogies_dist_thresh(v_protected)
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,32 @@
+def compute_bias_direction(rep_words=['he', 'she']):
+    """ Get the subspace that we will use to represent the bias. 
+    """
+    words_group1 = [rep_words[2 * i] for i in range(len(rep_words) // 2)]
+    words_group2 = [rep_words[2 * i + 1] for i in range(len(rep_words) // 2)]
+    E = WordEmbedding('embeddings/w2v_gnews_small.txt')
+    vs = [sum(E.v(w) for w in words) for words in (words_group2, words_group1)]
+    vs = [v / np.linalg.norm(v) for v in vs]
+
+    v_protected = vs[1] - vs[0]
+    v_protected = v_protected / np.linalg.norm(v_protected)
+    
+    return v_protected
+
+
+
+def main():
+    return "Hello"
+    parser = ArgumentParser()
+    parser.add_argument('--we_path')
+    parser.add_argument('--rep_words', default=['he', 'she'])
+    args = parser.parse_args()
+    
+    # Load word embedding from provided filepath
+    E = WordEmbedding(args.we_path)
+    
+    print(args.rep_words)
+    rep_words = ['he', 'she']
+    v_protected = compute_bias_direction(rep_words)
+
+    # Analogies based on the protected direction
+    a_protected = E.best_analogies_dist_thresh(v_protected)
