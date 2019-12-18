cb9d97c7e7446376ac731294fbcfb0bb590bdf68
Allie Rogers <arogers@coursera.org>
2019-12-17 21:50:55 -0800

    start css

---
+++ app/bias_explorer.py
@@ -0,0 +1,119 @@
+import numpy as np
+import os
+
+from argparse import ArgumentParser
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template
+from werkzeug.utils import secure_filename
+
+from debiaswe.debias import debias
+from debiaswe.we import WordEmbedding
+import model
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt'}
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            if not os.path.exists(app.config['UPLOAD_FOLDER']):
+                os.mkdir(app.config['UPLOAD_FOLDER'])
+            fp = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(fp)
+
+
+            return redirect(url_for('bias_explorer', filename=filename))
+
+    return render_template('home.html')
+
+
+@app.route('/bias_explorer/<filename>', methods=['GET', 'POST'])
+def bias_explorer(filename):
+    if request.method == 'POST':
+        fp = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        embedding = WordEmbedding(fp)
+        
+        rep_word_str = request.form['rep_word_str']
+        rep_words = rep_word_str.split(', ')
+        
+        v_protected = model.compute_bias_direction(embedding, rep_words)
+        
+        if request.form['submit_button'] == 'Analogies':
+            # Analogies based on the protected direction
+            a_protected = embedding.best_analogies_dist_thresh(v_protected)
+            print(a_protected)
+
+            return '''
+            <!doctype html>
+            <title>Bias Explorer</title>
+            <h1>Analogies</h1> 
+            <p>{a_protected}</p>
+            '''.format(a_protected=a_protected)
+    
+        if request.form['submit_button'] == 'Bias Scores':
+            wordset1, wordset2 = model.compute_bias_scores(embedding, v_protected)
+
+            return '''
+            <!doctype html>
+            <title>Bias Scores</title>
+            <h1>Welcome to the Bias Scores Page!</h1>
+            <p>{wordset1}</p>
+            <p>{wordset2}</p>
+            '''.format(wordset1=wordset1,
+                       wordset2=wordset2)
+        
+        if request.form['submit_button'] == 'Debiasing':
+            de = debias(embedding, 
+                        request.form['specific_words'].split(', '), 
+                        request.form['equalize_pairs'].split(', '),
+                        v_protected)
+            
+            de.save(os.path.join(app.config['UPLOAD_FOLDER'], 'debiased.txt'))
+            
+            return '''
+            <!doctype html>
+            <title>Debiased Embedding</title>
+            <h1>Your embedding has been debiased</h1>
+            <form action="/uploads/{filename}" method="POST">
+            <input type="hidden" name="param1" value="value1">
+            <input type="submit" name="submit_button" value="Download Me">
+            </form>
+            '''.format(filename=filename)       
+    
+    return render_template('exploration_page.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route('/uploads/<filename>', methods=['GET', 'POST'])
+def download(filename):
+    return send_from_directory(directory=app.config['UPLOAD_FOLDER'], 
+                               filename=filename, 
+                               as_attachment=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ app/model.py
@@ -0,0 +1,29 @@
+import numpy as np
+from debiaswe.data import load_professions
+from debiaswe.debias import debias
+from debiaswe.we import WordEmbedding
+
+def compute_bias_direction(embedding, rep_words=['he', 'she']):
+    """ Get the subspace that we will use to represent the bias. 
+    """
+    words_group1 = [rep_words[2 * i] for i in range(len(rep_words) // 2)]
+    words_group2 = [rep_words[2 * i + 1] for i in range(len(rep_words) // 2)]
+    vs = [sum(embedding.v(w) for w in words) for words in (words_group2, words_group1)]
+    vs = [v / np.linalg.norm(v) for v in vs]
+
+    v_protected = vs[1] - vs[0]
+    v_protected = v_protected / np.linalg.norm(v_protected)
+    
+    return v_protected
+
+def compute_bias_scores(embedding, v_protected):
+    # load professions
+    professions = load_professions()
+    profession_words = [p[0] for p in professions]
+    
+    # profession analysis gender
+    sp = sorted([(embedding.v(w).dot(v_protected), w) for w in profession_words])
+
+    return sp[0:20], sp[-20:]
+
+    
